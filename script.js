59fd7b5115fba49c2a7e1ec741f95c4ff43479fb
xaviguasch <xaviguasch@gmail.com>
2020-02-29 20:54:33 +0100

    added breathing animation with js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const container = document.getElementById('container')
+const text = document.getElementById('text')
 
+const totalTime = 7500
+
+const breatheTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+breathAnimation()
+
+function breathAnimation() {
+  text.innerText = 'Breathe In!'
+  container.className = 'container shrink'
+
+  setTimeout(() => {
+    text.innerText = 'Hold!'
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!'
+      container.className = 'container grow'
+    }, holdTime)
+  }, breatheTime)
+}
+
+setInterval(breathAnimation, totalTime)
